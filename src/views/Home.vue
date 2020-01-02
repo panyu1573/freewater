dbe04f308510c7a30d2b6a37122eba5d0f478e88
panyu <panyu@zhivai.com>
2020-01-03 01:35:29 +0800

    c

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home df fdc" style="position:fixed;width:100%;height:100%">
+    <div class="bf1 df fdc ac jc">
+      <div style="font: 120px 'Italiana', sans-serif">自由水</div>
+      <div style="font-size:16px" >网站开发中，敬请期待...</div>
+    </div>
+    
+    <a style="margin-bottom:20px" href="http://beian.miit.gov.cn/"  target="_blank">沪ICP备18023074号</a>
   </div>
 </template>
 
