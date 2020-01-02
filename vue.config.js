4586f231ec0b67312c53f6949efaa27bd6c18a67
panyu <panyu@zhivai.com>
2020-01-03 01:53:50 +0800

    c

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     devServer: {
         disableHostCheck: true,
+        public: 'http://47.111.253.252/',
       port: 80, // 端口
     },
     // lintOnSave: false // 取消 eslint 验证
